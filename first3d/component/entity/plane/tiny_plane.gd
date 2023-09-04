479353b7d422f99104b93f07f2f953f90cce6b6e
DricomDragon <dev@jovian-hersemeule.eu>
2023-09-04 18:30:09 +0200

    :wrench: Add plane controls

---
+++ first3d/component/entity/plane/tiny_plane.gd
@@ -0,0 +1,35 @@
+class_name TinyPlane
+extends RigidBody3D
+
+@export var thrust_power: float = 14000.0
+@export var turn_to_torque: float = 2000.0
+@export var move_to_pitch: float = 2000.0
+
+var target_torque: float = 0.0
+var target_pitch: float = 0.0
+var target_rotation := Vector3.ZERO
+var target_thrust := Vector3.ZERO
+
+
+func trigger_thrust() -> void:
+	target_thrust = Vector3.FORWARD * thrust_power
+
+
+func trigger_direction(dir: Vector2) -> void:
+	print(dir)
+	target_torque = -dir.x * turn_to_torque
+	target_pitch = -dir.y * move_to_pitch
+
+
+func _physics_process(delta: float) -> void:
+	apply_torque(transform * Vector3(target_pitch, 0.0, target_torque))
+	apply_central_force(target_thrust)
+
+
+func _on_dir_changed(dir: Vector2) -> void:
+	trigger_direction(dir)
+
+
+func _on_main_action() -> void:
+	print("Thrust !")
+	trigger_thrust()
