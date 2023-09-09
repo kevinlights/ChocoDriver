a46fd94f048fea1ba14d6f4f2cf296647bb137d8
DricomDragon <dev@jovian-hersemeule.eu>
2023-09-09 19:54:30 +0200

    :beetle: Fix non-responding walker move
    Trigger was based on joy motion.

--- first3d/flow/control/local_input/local_input.gd
+++ first3d/flow/control/local_input/local_input.gd
@@ -8,6 +8,6 @@ signal main_action(pressed: bool)
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action("jump"):
 		main_action.emit(event.is_pressed())
-	elif event is InputEventJoypadMotion:
+	elif event.is_action("move_left") or event.is_action("move_right") or event.is_action("move_back") or event.is_action("move_forward"):
 		var dir: Vector2 = Input.get_vector("move_left", "move_right", "move_back", "move_forward")
 		dir_changed.emit(dir)
