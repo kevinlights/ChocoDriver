bd626ba667b1c306ec38e44581c97c4ee5ad4b97
DricomDragon <dev@jovian-hersemeule.eu>
2023-09-10 10:12:35 +0200

    :truck: Rename jump as main
    Becaus triggering thruster is also main action.

--- first3d/flow/control/local_input/local_input.gd
+++ first3d/flow/control/local_input/local_input.gd
@@ -6,7 +6,7 @@ signal dir_changed(new_dir: Vector2)
 signal main_action(pressed: bool)
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action("jump"):
+	if event.is_action("main"):
 		main_action.emit(event.is_pressed())
 	elif event.is_action("move_left") or event.is_action("move_right") or event.is_action("move_back") or event.is_action("move_forward"):
 		var dir: Vector2 = Input.get_vector("move_left", "move_right", "move_back", "move_forward")
