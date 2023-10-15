f18bf9aec3f031428b75f464a25c35ae75a76ff7
DricomDragon <dev@jovian-hersemeule.eu>
2023-10-15 19:37:38 +0200

    :fire: Remove crash debug

--- godot/effect/crash/crash.gd
+++ godot/effect/crash/crash.gd
@@ -2,14 +2,5 @@ class_name CrashEffect
 extends Node3D
 
 
-func _init():
-	print("Crash init")
-
-
-func _ready():
-	print("Crash ready")
-
-
 func _on_noise_finished():
-	print("Crash end")
 	queue_free()
