47b29009467ecdfb2a24fa94995787715bb7b6fa
DricomDragon <dev@jovian-hersemeule.eu>
2023-10-15 20:00:40 +0200

    :beetle: Fix crash destroyed before particles completed

--- godot/effect/crash/crash.gd
+++ godot/effect/crash/crash.gd
@@ -2,5 +2,20 @@ class_name CrashEffect
 extends Node3D
 
 
-func _on_noise_finished():
-	queue_free()
+var _noise_finished: bool = false
+var _splash_finished: bool = false
+
+
+func _on_noise_finished() -> void:
+	_noise_finished = true
+	_die_if_completed()
+
+
+func _on_splash_finished() -> void:
+	_splash_finished = true
+	_die_if_completed()
+
+
+func _die_if_completed() -> void:
+	if _noise_finished and _splash_finished:
+		queue_free()
--- godot/effect/crash/splash_gfx.gd
+++ godot/effect/crash/splash_gfx.gd
@@ -1,5 +1,14 @@
 extends GPUParticles3D
 
 
+signal finished
+
+
 func _ready() -> void:
 	set_emitting(true)
+	var emission_timer: SceneTreeTimer = get_tree().create_timer(get_lifetime(), false, true)
+	emission_timer.timeout.connect(_on_emission_ended)
+
+
+func _on_emission_ended() -> void:
+	finished.emit()
