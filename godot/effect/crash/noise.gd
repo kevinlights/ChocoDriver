0995a44c980a9a462e65980922dd084d99a30650
DricomDragon <dev@jovian-hersemeule.eu>
2023-10-16 20:47:27 +0200

    :wrench: Randomize crash sound resource
    For each impact.

--- godot/effect/crash/noise.gd
+++ godot/effect/crash/noise.gd
@@ -1,8 +1,18 @@
 extends AudioStreamPlayer3D
 
-const crash_sound: Resource = preload("res://effect/crash/crash.wav")
+const crash_sounds: Array[AudioStream] = [
+	preload("res://effect/crash/crash0.wav"),
+	preload("res://effect/crash/crash1.wav"),
+	preload("res://effect/crash/crash2.wav"),
+	preload("res://effect/crash/crash3.wav"),
+]
 
 
 func _ready() -> void:
-	stream = crash_sound
+	stream = _pick_random_sound()
 	play()
+
+
+func _pick_random_sound() -> AudioStream:
+	var rand_index: int = randi_range(0, crash_sounds.size() - 1)
+	return crash_sounds[rand_index]
